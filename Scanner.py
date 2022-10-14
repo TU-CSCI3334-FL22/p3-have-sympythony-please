ba00fac21492aa81be3e5fd07118a4263d3397f0
Nathan Early <nearly@trinity.edu>
2022-10-13 20:12:57 -0500

    scanner complete

--- Scanner.py
+++ Scanner.py
@@ -1,21 +1,63 @@
+from enum import Enum
+from lib2to3.pgen2 import token
+from mailbox import linesep
+
+class Token(Enum):
+    SEMICOLON = 1
+    DERIVES = 2
+    ALSODERIVES = 3
+    EPSILON = 4
+    SYMBOL = 5
+
+token_map = {
+    ':': Token.DERIVES,
+    ';': Token.SEMICOLON,
+    '|': Token.ALSODERIVES,
+    'epsilon': Token.EPSILON,
+}
+
 
 def scanner(filename):
-    file = open(filename, "r")
-    # loop through the file
-    # collect all the symbols into a list first
-    
-    
-    symbols = []
-    non_terminals = []
+    """Main Scanner Function
 
+    Args:
+        filename (string): name of file we are scanning
+
+    Returns:
+        tuple(list[list[Token]], dict[string,int]): 
+        (list of tokens, lookup table for symbols)
+    """
+    # read in the file
+    file = open(filename, "r")
+    
+    # dict to represent the lookup table for symbols
+    lookup_tbl = {}
+
+    # 2D list of tokens for entire file
+    lines = []
     for line in file:
+        
+        # output for current line
+        tokens = []
+
+        # each token in the line split on whitespace
         parts = line.split()
         for part in parts:
-            term = part.lower()
-            if term == "|":
-                
-            elif term == ";":
-            elif term == ":":
-            elif term == "epsilon":
-            else:
+            # converting to lower to simplify checks
+            # basically just for handling diff caps for epsilon
+            token = part.lower()
 
+            # if we encounter a token that isnt accounted for in lookup table, add it
+            if token not in token_map and part not in lookup_tbl:
+                # using len to assign idx instead of storing a local idx var
+                lookup_tbl[part] = len(lookup_tbl)
+
+            # append the token to the list of tokens
+            tokens.append(token_map.get(token, Token.SYMBOL))
+            
+        # if the list isnt empty append it
+        if tokens:
+            lines.append(tokens)
+        
+    # return type: (list[list[Token]], dict(string,int))
+    return (lines,lookup_tbl)
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+import string
 from Scanner import scanner
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from argparse import ArgumentParser, FileType
+
+# read arguments from commandline
+# rn only takes one argument, filename
+parser = ArgumentParser(description="Scans tokens from a grammar.")
+parser.add_argument('filename', type=str, metavar='F',
+                    help="name of file to be scanned")
+args = parser.parse_args()
 
 def main():
-    """ Main program """
-    # Code goes over here.
-    print("main ran")
-    scanner("grammars/CEG-RR")
+    # extract filename from args
+    filename = args.filename
+
+    # pass into scanner and pattern-match output
+    tokens,lookup_tbl = scanner(filename)
+    for line in tokens:
+        # enums in python have a builtin .name that returns string
+        print(list(map(lambda x: x.name, line)))
+    
+    print(lookup_tbl)
+    
     return 0
 
 if __name__ == "__main__":
