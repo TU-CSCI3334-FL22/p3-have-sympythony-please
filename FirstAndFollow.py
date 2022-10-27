4cb6fdb274946aea12dce7d0fdbd25ee394410f6
2020-09-06 <nearly@atlas07.cs.trinity.edu>
2022-10-27 13:51:28 -0500

    something working with nextTbl

--- FirstAndFollow.py
+++ FirstAndFollow.py
@@ -48,7 +48,7 @@ def computeFirsts(g):
             # rep epsilon using "ε"
             # rep eof using ""
 
-            rhs = t.firstTable[prods[0]]
+            rhs = t.firstTable[prods[0]].copy()
             rhs.discard(_epsilon)
 
             k = len(prods)
@@ -106,67 +106,45 @@ def computeFollows(g, t):
                 else:
                     trailer = t.firstTable[prods[i]]
 
-    for k,v in t.followTable.items():
-        print(k + ": ", end="")
-        print(v)
+    # for k,v in t.followTable.items():
+    #     print(k + ": ", end="")
+    #     print(v)
 
     return t
 
 def computeNext(g,t):
-    
-    t_set = set(g.terminals)
-    # sets contain first termianls that can come out of rules
     for i in range(len(g.productions)):
         t.nextTable[i] = set()
-    
-    for i in range(0, len(g.productions)):
-        idx,prod_name, prods = g.productions[i]
+
+    for idx, prod_name,prods in g.productions:
+        if not prods:
+            t.firstTable[prod_name].add(_epsilon)
+            # AND the follow set of prod_Name
+            t.firstTable[prod_name].update(t.followTable[prod_name])
+            continue
+
+        rhs = t.firstTable[prods[0]].copy()
+        rhs.discard(_epsilon)
+
+        k = len(prods)
+        i = 0
+        while i < k and _epsilon in t.firstTable[prods[i]]:
+            for x in t.firstTable[prods[i]]:
+                if x != _epsilon:
+                    rhs.add(x)
+            i += 1
+            
+        if i == k and _epsilon in t.firstTable[prods[k-1]]:
+            rhs.add(_epsilon) #AND the follow of prod_name
+            rhs.update(t.followTable[prod_name])
+
         
-        if all(list(map(lambda x: _epsilon in x, prods))):
-            for prod in prods:
-                t.nextTable[i].update(t.firstTable[prod])
-            t.nextTable[i].update(t.followTable[prod_name])
-
-
-        # if prod_name in t_set:
-        #     t.nextTable[i] = prod_name
-        # else:
-        #     t.nextTable[i].update(t.firstTable[prod_name])       
-        #     t.nextTable[i].discard(_epsilon)
-
-
+        t.nextTable[idx].update(rhs)
+        # for x in rhs:
+        #     t.nextTable[i].add(x)
     
-    print(t.nextTable)
+    for k,v in t.nextTable.items():
+        print(f"{k}: ", end="")
+        print(v)
 
     return t
-
-
-
-# def exampleNext(g,t):
-#     for i, prod_name,prods in reversed(g.productions):
-#             prods = list(map(lambda x: x[1], prods))
-#             # print(prods)
-#             if not prods:
-#                 t.firstTable[prod_name].add(_epsilon) # AND the follow set of prod_Name
-#                 continue
-            
-#             # do we have to handle prod_lst \ {e}
-#             # rep epsilon using "ε"
-#             # rep eof using ""
-
-#             rhs = t.firstTable[prods[0]]
-#             rhs.discard(_epsilon)
-
-#             k = len(prods)
-#             i = 0
-#             while i < k and _epsilon in t.firstTable[prods[i]]:
-#                 for x in t.firstTable[prods[i]]:
-#                     if x != _epsilon:
-#                         rhs.add(x)
-#                 i += 1
-                
-#             if i == k and _epsilon in t.firstTable[prods[k-1]]:
-#                 rhs.add(_epsilon) #AND the follow of prod_name
-
-#             for x in rhs:
-#                 t.nextTable[i].add(x)
